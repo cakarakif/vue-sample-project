045b72182d07f941f06f4f6095ecbc0c120f32d3
AKİF ÇAKAR <akif.cakar@ceng.deu.edu.tr>
2021-09-25 12:21:31 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <app-header></app-header>
+            <br>
+            <router-view></router-view>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+    components:{
+        appHeader : Header
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul class="nav nav-pills">
+        <router-link 
+                active-class="active"
+                exact
+                tag="li"
+                to="/">
+            <a href="">Home</a>
+        </router-link>
+        <router-link 
+                active-class="active"
+                exact
+                tag="li"
+                to="/user/1">
+            <a href="">User 1</a>
+        </router-link>
+        <router-link 
+                active-class="active"
+                exact
+                tag="li"
+                to="/user/2">
+            <a href="">User 2</a>
+        </router-link>
+    </ul>
+</template>
